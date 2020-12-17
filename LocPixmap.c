588bd0c669dce898df5964341c7b1e770a8a4cb7
Yoshihiro Takahashi <nyan@furiru.org>
2020-12-17 20:38:36 +0900

    Fix build.

--- LocPixmap.c
+++ LocPixmap.c
@@ -30,6 +30,7 @@
 #include <X11/Xlib.h>
 #include <X11/Xresource.h>
 #include <X11/Xutil.h>
+#include <X11/Intrinsic.h>
 #include <X11/StringDefs.h>
 #include <sys/param.h>			/* get MAXPATHLEN if possible */
 #ifndef MAXPATHLEN
